fd9cc35e0a21c2c95e555e1345feaa9f34da804b
Shashank Tewari <RGTK86@motorolasolutions.com>
2021-03-05 08:58:20 +0530

    Commiting3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,8 +3,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("Hello world1");
-        System.out.println("Hello world3");
-        System.out.println("Hello wor44");
+        System.out.println("Hello world For Docker");
     }
 }
